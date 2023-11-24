686f9c2613dbb76a8123cfabcbbe0e93f24e8516
pavo-etc <z1verdeman2@gmail.com>
2023-11-24 13:27:37 +1100

    Add windows support

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ import { exec } from "child_process";
 import { App, Notice, Plugin } from "obsidian";
 import * as path from "path";
 
+var isWin = process.platform === "win32";
+
 export default class DeployOchrsPlugin extends Plugin {
 	async onload() {
 		// This creates an icon in the left ribbon.
@@ -29,22 +31,47 @@ const deploy = (app: App) => {
 
 	const file = app.vault.getFiles().find((f) => f.name === "Index.md");
 	if (file) {
-		const text = app.vault.getResourcePath(file);
-		const indexPath = text.split("Index.md")[0].slice(11);
-		const vaultDir = path.dirname(indexPath.slice(indexPath.indexOf("/")));
-		exec(
-			`${vaultDir}/update-notes.sh`,
-			{ cwd: vaultDir },
-			(error, stdout, stderr) => {
-				if (error) {
-					new Notice(error.message);
-					return;
+		if (isWin) {
+			const text = app.vault.getResourcePath(file);
+			const indexPath = text.split("Index.md")[0].slice(11);
+			const vaultDir = path.dirname(
+				indexPath.slice(indexPath.indexOf("/"))
+			)
+				.replace("/","")
+				.replace(/\//g,"\\");
+
+			exec(
+				"bash -c ./update-notes.sh",
+				{ cwd: vaultDir },
+				(error, stdout, stderr) => {
+					if (error) {
+						new Notice(error.message);
+						return;
+					}
+					new Notice(stdout);
+					const msg = "Finished deploying!";
+					new Notice(msg);
 				}
-				new Notice(stdout);
-				const msg = "Finished deploying!";
-				new Notice(msg);
-			}
-		);
+			);
+		} else {
+			const text = app.vault.getResourcePath(file);
+			const indexPath = text.split("Index.md")[0].slice(11);
+			const vaultDir = path.dirname(indexPath.slice(indexPath.indexOf("/")));
+	
+			exec(
+				`${vaultDir}/update-notes.sh`,
+				{ cwd: vaultDir },
+				(error, stdout, stderr) => {
+					if (error) {
+						new Notice(error.message);
+						return;
+					}
+					new Notice(stdout);
+					const msg = "Finished deploying!";
+					new Notice(msg);
+				}
+			);
+		}
 	} else {
 		new Notice("No index found");
 	}
